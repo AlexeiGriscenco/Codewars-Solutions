27c4a4857d901143351f631bd7886b4d3d86feb7
Alexei Griscenco <alexeygr3000@mail.ru>
2022-06-06 23:23:44 +0300

    added new codewars challenge

---
+++ 7kyu/Reverse Letter.hs
@@ -0,0 +1,24 @@
+{-
+Task
+
+Given a string str, reverse it omitting all non-alphabetic characters.
+Example
+
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+Input/Output
+
+    [input] string str
+
+A string consists of lowercase latin letters, digits and symbols.
+
+    [output] a string
+-}
+
+module SF176 where
+
+import           Data.Char (isAlpha)
+
+reverseLetter :: String -> String
+reverseLetter = reverse . filter isAlpha
